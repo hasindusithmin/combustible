6a342e681d58145694c4cab8c224139dd94635e6
hasindusithmin <hasindusithmin64@gmail.com>
2023-01-05 20:06:20 +0530

    Delete routes/logout.js

--- index.js
+++ index.js
@@ -9,14 +9,23 @@ app.use(cookieParser())
 
 app.get('/', (req, res) => res.sendStatus(200))
 
-mongoose.set('strictQuery',true)
+app.get('/logout', (req, res) => {
+    try {
+        res.cookie('JWT', '', { maxAge: 0 })
+        res.redirect('/')
+    } catch (error) {
+        res.status(500).json({ ERROR: error.message })
+    }
+})
+
+mongoose.set('strictQuery', true)
 mongoose.connect(process.env.MONGO_URL)
-    .then(()=>{
-        app.listen(3000,()=>{
+    .then(() => {
+        app.listen(3000, () => {
             console.log('Server Running Port:3000');
         })
     })
-    .catch(({message})=>{
+    .catch(({ message }) => {
         console.error(message);
     })
 
