000e532ae72a48e86b3192347e4af294e4d09f9a
hasindusithmin <hasindusithmin64@gmail.com>
2023-01-05 21:06:02 +0530

    Create controllers/exists/*

---
+++ controllers/exists/after.js
@@ -0,0 +1,21 @@
+
+const StationOwnerModel = require("../../models/station-owner")
+const VehicleOwnerModel = require("../../models/vehicle-owner")
+
+module.exports = async function handler(req,res) {
+    try {
+        const {id,username,qty,fuel,user_id} = req.body;
+        if (id === undefined) throw Error("id (query) is required.")
+        const station = await StationOwnerModel.findOne({_id:id})
+        const queue = station['QUEUE'].filter(({USERNAME})=>USERNAME !== username)
+        let DIESEL  = station['DIESEL']
+        let PETROL  = station['PETROL']
+        if (fuel === 'DIESEL') DIESEL -= parseInt(qty)
+        if (fuel === 'PETROL') PETROL -= parseInt(qty)
+        await StationOwnerModel.updateOne({_id:id},{$set:{QUEUE:queue,DIESEL,PETROL}})
+        await VehicleOwnerModel.updateOne({_id:user_id},{$set:{QUEUE:""}})
+        res.status(200).json({})
+    } catch (error) {
+        res.status(500).json({ERROR:error.message})
+    }
+}
---
+++ controllers/exists/before.js
@@ -0,0 +1,19 @@
+
+
+
+const StationOwnerModel = require("../../models/station-owner")
+const VehicleOwnerModel = require("../../models/vehicle-owner")
+
+module.exports =  async function handler(req,res) {
+    try {
+        const {id,username,user_id} = req.body;
+        if (id === undefined) throw Error("id (query) is required.")
+        const station = await StationOwnerModel.findOne({_id:id})
+        const queue = station['QUEUE'].filter(({USERNAME})=>USERNAME !== username)
+        await StationOwnerModel.updateOne({_id:id},{$set:{QUEUE:queue}})
+        await VehicleOwnerModel.updateOne({_id:user_id},{$set:{QUEUE:""}})
+        res.status(200).json({})
+    } catch (error) {
+        res.status(500).json({ERROR:error.message})
+    }
+}
